fb018ab085b32d20b9c33548c65737a36b5966d4
MikolajChojnacki <mikolaj.chojnacki@wp.pl>
2020-04-07 11:58:09 +0200

    Ready square tool

--- Dev/Js/ToolFabric/Tools/Square.js
+++ Dev/Js/ToolFabric/Tools/Square.js
@@ -2,6 +2,7 @@ class Square {
     constructor() {
         this.name = `square`;
         this.color = `rgba(90, 171,225,0.7)`;
+        this.lineWidth = `5`;
         this.isDrawing = false;
         this.startPoint = {
             x: 0,
@@ -20,34 +21,25 @@ class Square {
 
         image.src = canvas.toDataURL(`image/png`);
 
-        this.canvasDrawingInitialState = image;
         this.isDrawing = true;
+        this.canvasDrawingInitialState = image;
         this.startPoint.x = x - canvas.offsetLeft;
         this.startPoint.y = y - canvas.offsetTop;
 
     }
 
     onMouseMove(canvas, x, y) {
+
         if (this.isDrawing) {
+            let ctx = canvas.getContext(`2d`);
 
-            window.requestAnimationFrame(() => {
-                let ctx = canvas.getContext(`2d`);
+            this.endPoint.x = x - canvas.offsetLeft;
+            this.endPoint.y = y - canvas.offsetTop;
 
-                this.endPoint.x = x - canvas.offsetLeft;
-                this.endPoint.y = y - canvas.offsetTop;
+            ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+            ctx.drawImage(this.canvasDrawingInitialState, 0, 0);
 
-                ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
-                ctx.drawImage(this.canvasDrawingInitialState, 0, 0);
-
-                ctx.fillStyle = this.color;
-                ctx.fillRect(
-                    this.startPoint.x,
-                    this.startPoint.y,
-                    this.endPoint.x - this.startPoint.x,
-                    this.endPoint.y - this.startPoint.y
-                )
-
-            })
+            this.drawSquare(ctx);
         }
     }
 
@@ -61,13 +53,7 @@ class Square {
         ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
         ctx.drawImage(this.canvasDrawingInitialState, 0, 0);
 
-        ctx.fillStyle = this.color;
-        ctx.fillRect(
-            this.startPoint.x,
-            this.startPoint.y,
-            this.endPoint.x - this.startPoint.x,
-            this.endPoint.y - this.startPoint.y
-        )
+        this.drawSquare(ctx);
 
     }
 
@@ -77,6 +63,33 @@ class Square {
 
     updateFontSize() { }
 
+    drawSquare(ctx) {
+        window.requestAnimationFrame(() => {
+            ctx.strokeStyle = this.color;
+            ctx.lineWidth = this.lineWidth;
+
+            // left
+            ctx.beginPath();
+            ctx.moveTo(this.startPoint.x, this.startPoint.y);
+            ctx.lineTo(this.startPoint.x, this.endPoint.y);
+            ctx.stroke();
+            //top
+            ctx.beginPath();
+            ctx.moveTo(this.startPoint.x, this.startPoint.y);
+            ctx.lineTo(this.endPoint.x, this.startPoint.y);
+            ctx.stroke();
+            // right
+            ctx.beginPath();
+            ctx.moveTo(this.endPoint.x, this.startPoint.y);
+            ctx.lineTo(this.endPoint.x, this.endPoint.y);
+            ctx.stroke();
+            // bottom
+            ctx.beginPath();
+            ctx.moveTo(this.startPoint.x, this.endPoint.y);
+            ctx.lineTo(this.endPoint.x, this.endPoint.y);
+            ctx.stroke();
+        });
+    }
 }
 
 module.exports = {
